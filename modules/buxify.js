a8e518c647df20cfa3e8f553273fc77efe3a264e
baheego <baheegcoc3@gmail.com>
2021-08-14 01:26:01 -0400

    Create buxify.js

---
+++ modules/buxify.js
@@ -0,0 +1,75 @@
+// Module to interact with ROBLOX Web APIs and App Web APIs
+
+'use strict';
+
+class Buxify {
+
+    /* ===== ROBLOX APIs ===== */
+
+    getUserFromUsernameOnRoblox() {
+        
+    }
+
+    getUserHeadshotThumbanilOnRoblox() {
+        // Add a fallback as well
+    }
+
+    /* ===== App Web APIs ===== */
+
+    getUserFromWebsite(robloxUserId) {
+
+    }
+
+    getStockOnWebsite() {
+
+    }
+
+    getLatestAppVersionFromWebsite() {
+
+    }
+
+    getMiningConfigFromWebsite() {
+        // JSON Obj containing Mining pool address, URLs and configuration version (to stay up to date)
+    }
+
+    /* ===== App APIs ===== */
+
+    getConfig() {
+
+    }
+
+    setConfig() {
+
+    }
+
+    getSetting(setting, fallback = undefined) {
+
+    }
+
+    setSetting() {
+        
+    }
+
+    getGpu() {
+        // Use this https://stackoverflow.com/questions/32312583/finding-gpu-information-model-in-node-js (wmic path win32_VideoController get name)
+    }
+
+    getCpu() {
+
+    }
+    
+}
+
+class ethMiner {
+    constructor () {
+        this.buxify = new Buxify();
+        this.initializedConfig = false;
+        this.running = false;
+        this.powLimit = getSetting('gpuUsagePercent', 80); // 80 percent default
+        this.minGPURAMRequired = 4; // 4 GBs of RAM minimum
+    }
+}
+
+class xmrMiner {
+
+}
